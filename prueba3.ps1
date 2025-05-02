b0b973c3c63e83651733584b3597bc56f7f58d6d
mirapuedovolar2 <cuentaparametasimon@gmail.com>
2025-05-02 15:52:32 -0400

    Delete prueba3.ps1

--- prueba3.ps1
+++
@@ -1,7 +0,0 @@
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
-$url = "https://github.com/mirapuedovolar2/ps1prueba/releases/download/hola2/este2.exe"
-$destino = "$env:TEMP\este2.exe"
-
-Invoke-WebRequest -Uri $url -OutFile $destino
-Start-Process -FilePath $destino
